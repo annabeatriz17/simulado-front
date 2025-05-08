9fde84b1fb67585c13bfceb4166a5b107f373475
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-05-08 10:51:03 -0300

    update

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function Home() {
     return (
         <div className={styles.container}>
-            <Image className={styles.image} src="/images/maiko.png" alt="Maiko Xikixiki" />
+            <Image className={styles.image} src="/images/maiko.png" alt="Maiko Xikixiki" width={100} height={100} />
             <h1 className={styles.title}> Maiko Xikixiki Bahia</h1>
             <div className={styles.description}>
                 <p>Tá perdido no código? Relaxa e vem de Maiko, que vamos te mostrar a usar:</p>

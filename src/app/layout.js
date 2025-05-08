f82ebebdf8a203e88eadf243acf565ddf478c87c
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-05-08 10:36:59 -0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,24 @@
+import React from "react";
 import "./globals.css";
+import { Roboto } from "next/font/google";
 
 export const metadata = {
     title: "My Exam Mockup",
     description: "Projeto pra mostrar tudo que eu sei",
 };
 
+const font = Roboto({
+    variable: "--font",
+    subsets: ["latin"],
+});
+
 export default function RootLayout({ children }) {
     return (
-        <html lang="pt-BR">
-            <body>{children}</body>
+        <html lang="pt-BR" className={font.variable}>
+            <body className={font.className}>{children}</body>
+            <head>
+                <link rel="icon" href="/icons/favicon.ico" />
+            </head>
         </html>
     );
 }
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "../styles/Loading.module.css";
+import Image from "next/image";
 
 export default function Loading() {
     return (
         <div className={styles.loading}>
-            <img src="/images/world.gif" alt="Loading" />
+            <Image src="/images/loading.gif" alt="Carregando" width={100} height={100} className={styles.image} />
             <p>Carregando países...</p>
         </div>
     );
